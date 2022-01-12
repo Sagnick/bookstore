943784bad49a4608207af53db6744d31c4bb8b36
Sagnick Banerjee <sagnickbanerjee4u@gmail.com>
2022-01-12 19:33:51 +0530

    Modified Test Case

--- src/test/java/com/demp/sb/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/demp/sb/bookstore/BookstoreApplicationTests.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.demo.sb.bookstore.BookStoreApplication;
+
+@SpringBootTest(classes = BookStoreApplication.class)
 class BookstoreApplicationTests {
 
 	@Test
